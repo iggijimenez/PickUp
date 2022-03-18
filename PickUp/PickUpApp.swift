2cf4444369bd06460baa9bc7d3b3d9173902ccbd
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2022-03-17 23:18:01 -0700

    Initial Commit

---
+++ PickUp/PickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUpApp.swift
+//  PickUp
+//
+//  Created by jimenez on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

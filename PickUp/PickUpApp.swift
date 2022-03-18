48f245c38baf36059904430127545867389f9cc2
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2022-03-18 00:32:22 -0700

    Working with firebase

--- PickUp/PickUpApp.swift
+++ PickUp/PickUpApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PickUpApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
